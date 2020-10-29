95be1e3390d1eeee0c1e158811b917bf920c93cf
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-10-29 22:59:08 +0530

    Update face.js

--- src/api/face.js
+++ src/api/face.js
@@ -1,6 +1,6 @@
 import * as faceapi from 'face-api.js';
 
-
+// Load models and weights
 export async function loadModels() {
   const MODEL_URL = process.env.PUBLIC_URL + '/models';
   await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
@@ -9,7 +9,7 @@ export async function loadModels() {
 }
 
 export async function getFullFaceDescription(blob, inputSize = 512) {
-
+  // tiny_face_detector options
   let scoreThreshold = 0.5;
   const OPTION = new faceapi.TinyFaceDetectorOptions({
     inputSize,
@@ -17,9 +17,11 @@ export async function getFullFaceDescription(blob, inputSize = 512) {
   });
   const useTinyModel = true;
 
-
+  // fetch image to api
   let img = await faceapi.fetchImage(blob);
 
+  // detect all faces and generate full description from image
+  // including landmark and descriptor of each face
   let fullDesc = await faceapi
     .detectAllFaces(img, OPTION)
     .withFaceLandmarks(useTinyModel)
@@ -27,4 +29,24 @@ export async function getFullFaceDescription(blob, inputSize = 512) {
   return fullDesc;
 }
 
+const maxDescriptorDistance = 0.5;
+export async function createMatcher(faceProfile) {
+  // Create labeled descriptors of member from profile
+  let members = Object.keys(faceProfile);
+  let labeledDescriptors = members.map(
+    member =>
+      new faceapi.LabeledFaceDescriptors(
+        faceProfile[member].name,
+        faceProfile[member].descriptors.map(
+          descriptor => new Float32Array(descriptor)
+        )
+      )
+  );
 
+  // Create face matcher (maximum descriptor distance is 0.5)
+  let faceMatcher = new faceapi.FaceMatcher(
+    labeledDescriptors,
+    maxDescriptorDistance
+  );
+  return faceMatcher;
+}
